3279b54a5b66c6bb3a1d5dce062493ce411af3c4
karan pun <karanpun011@gmail.com>
2025-03-03 17:25:14 -0500

    tailwind v4 installation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,8 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className=' text-xl font-bold underline'>Vite + React</h1>
+      <div className="flex text-4xl">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p  className="read-the-docs ">
         Click on the Vite and React logos to learn more
       </p>
     </>
