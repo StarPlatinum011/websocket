6ab0891a6489a3138a76d78a353651274c1b578d
karan pun <karanpun011@gmail.com>
2025-12-12 14:02:16 -0500

    add prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -39,8 +39,7 @@ wss.on('connection', (ws) => {
             timestamp: new Date().toLocaleTimeString('en-US', {
                     hour:'numeric',
                     minute: '2-digit',
-                    second:'2-digit',
-                    hour12: false
+                    hour12: true
                 }),
         });
 
