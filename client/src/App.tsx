740b777c4c8177c646416c4e88149298299502db
karan pun <karanpun011@gmail.com>
2026-01-27 22:05:01 -0500

    add router structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import MessengerUI from './pages/MessengerUI'
 import './style.css'
 function App() {
 
   return (
     <BrowserRouter>
-      <MessengerUI />
+      <Routes>
+          <Route path="/" element={<MessengerUI />} />
+          <Route path="/room/:roomId" element={<MessengerUI />} />
+          <Route path="/404" element={<div>404 Not Found.</div>} />
+          <Route path="*" element={<Navigate to={'/404'} replace/>} />
+      </Routes>
     </BrowserRouter>
   )
 }
